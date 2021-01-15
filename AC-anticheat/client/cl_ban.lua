74369d246a9eb31afef5f71449b9fa87bf6a2aef
cold-cat <62061588+cold-cat@users.noreply.github.com>
2021-01-15 16:59:46 +0330

    Delete AC-anticheat directory

--- AC-anticheat/client/cl_ban.lua
+++
@@ -1,8 +0,0 @@
-RegisterNetEvent('BanSql:Respond')
-AddEventHandler('BanSql:Respond', function()
-	TriggerServerEvent("BanSql:CheckMe")
-end)
---[[RegisterNetEvent('ban:plsban')
-AddEventHandler('ban:plsban', function(ress)
-	TriggerServerEvent('ban:banserver', ress)
-end)]]
